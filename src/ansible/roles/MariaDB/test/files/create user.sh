0531f236ca8c7ca73f8c3e698b49ecbe4ef01914
🦄Pegasus (DarkStar☀) <darkstar@unisocial.net>
2023-07-16 18:59:43 -0500

    try

--- src/ansible/roles/MariaDB/test/files/create user.sh
+++ src/ansible/roles/MariaDB/test/files/create user.sh
@@ -1,9 +1,15 @@
 # https://www.digitalocean.com/community/tutorials/how-to-create-a-new-user-and-grant-permissions-in-mysql
+# https://ikasten.io/2017/01/08/whats-the-default-password-of-root-user-in-mariadb/
 
+# Login without password
 sudo mysql -u root
 
 CREATE USER 'user1'@'localhost' IDENTIFIED BY 'password';
 
+# Grant Permission
 GRANT ALL PRIVILEGES ON *.* TO 'user1'@'localhost' WITH GRANT OPTION;
 
 FLUSH PRIVILEGES;
+
+# Test new user
+sudo mysql -u user1 -p
